9430a84d7be576c03d0d2976ec9427c71cdf6df3
spencerwooo <spencerwoo98@foxmail.com>
2019-03-09 14:51:25 +0800

    🎾 Update: Python random generate

--- Python/src/matrix.py
+++ Python/src/matrix.py
@@ -1,14 +1,20 @@
 #! python3
 
 import numpy
+import time
 
 
 def main():
-    matrix = numpy.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-    vector = numpy.array([1, 2, 3])
-    product1 = numpy.dot(vector, matrix)
-    product2 = numpy.dot(matrix, vector)
-    print(product1, product2)
+    matrix = numpy.random.randint(10, size=(10000, 10000))
+    vector = numpy.random.randint(10, size=(1, 10000))
+
+    startTime = time.time()
+    product = numpy.dot(vector, matrix)
+    print(product)
+    endTime = time.time()
+
+    totalTime = format(endTime - startTime, '.5f')
+    print('Total time: ', totalTime)
 
 
 if __name__ == "__main__":
