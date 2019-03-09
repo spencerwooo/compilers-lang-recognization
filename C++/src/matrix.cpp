4f1f46ae01552d0f9873fc3189138af5479b7383
spencerwooo <spencerwoo98@foxmail.com>
2019-03-09 16:03:43 +0800

    🧢 Update: C++

--- C++/src/matrix.cpp
+++ C++/src/matrix.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <ctime>
 
-using namespace std;
-
-void printMatrix(vector<vector<int>> matrix)
+void printMatrix(std::vector<std::vector<int>> matrix)
 {
   int matrixRow = int(matrix.size());
   int matrixCol = int(matrix[0].size());
@@ -12,23 +11,23 @@ void printMatrix(vector<vector<int>> matrix)
   {
     for (int j = 0; j < matrixCol; j++)
     {
-      cout << matrix[i][j] << " ";
+      std::cout << matrix[i][j] << " ";
     }
-    cout << endl;
+    std::cout << std::endl;
   }
 }
 
-vector<vector<int>> multiplyMatrix(vector<vector<int>> matrix1, vector<vector<int>> matrix2)
+std::vector<std::vector<int>> multiplyMatrix(std::vector<std::vector<int>> matrix1, std::vector<std::vector<int>> matrix2)
 {
   for (int i = 0; i < int(matrix1.size()); i++)
   {
     if (int(matrix1[i].size()) != int(matrix2.size()))
     {
-      throw invalid_argument("[ERR] Cannot multiply!");
+      throw std::invalid_argument("[ERR] Cannot multiply!");
     }
   }
 
-  vector<vector<int>> resultMatrix(int(matrix1.size()), vector<int>(int(matrix2[0].size()), 0));
+  std::vector<std::vector<int>> resultMatrix(int(matrix1.size()), std::vector<int>(int(matrix2[0].size()), 0));
 
   for (int i = 0; i < int(resultMatrix.size()); i++)
   {
@@ -46,21 +45,39 @@ vector<vector<int>> multiplyMatrix(vector<vector<int>> matrix1, vector<vector<in
 
 int main(int argc, char const *argv[])
 {
-  vector<vector<int>> matrix1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-  vector<vector<int>> matrix2 = {{1, 2, 3}};
+  srand(time(NULL));
 
-  printMatrix(matrix1);
-  printMatrix(matrix2);
+  std::vector<std::vector<int>> matrix1;
+  std::vector<std::vector<int>> matrix2;
+
+  for (int i = 0; i < 10000; i++) {
+    matrix1.push_back(std::vector<int>());
+    for (int j = 0; j < 10000; j++) {
+      matrix1[i].push_back(rand() % 10);
+    }
+  }
+
+  for (int i = 0; i < 1; i++) {
+    matrix2.push_back(std::vector<int>());
+    for (int j = 0; j < 10000; j++) {
+      matrix2[i].push_back(rand() % 10);
+    }
+  }
+
+  double startTime = clock();
 
   try
   {
-    vector<vector<int>> result = multiplyMatrix(matrix2, matrix1);
-    printMatrix(result);
+    std::vector<std::vector<int>> result = multiplyMatrix(matrix2, matrix1);
   }
   catch (const std::exception &e)
   {
     std::cerr << e.what() << '\n';
   }
+
+  double endTime = clock();
+  double totalTime = endTime - startTime;
+  std::cout << "Total time: " << totalTime << std::endl;
 
   return 0;
 }
